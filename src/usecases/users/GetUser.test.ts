98095cb8003d19481b7ad40e8e5e422a2f086f12
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-12-26 20:22:51 -0300

    refactoring get user use case unit test

--- src/usecases/users/GetUser.test.ts
+++ src/usecases/users/GetUser.test.ts
@@ -1,59 +1,42 @@
-import { UserMapper, YupUserValidator } from "../../infra/adapters";
 import { UserInMemoryRepository } from "../../infra/repositories";
-import { CreateUserUseCase, CreateUserUseCaseProps } from "./CreateUser";
 import { getThrowedErrorType, NoErrorThrownError } from "../../tests";
 import { NotFoundException } from "../../domain/exceptions";
-import { GetUserUseCase, GetUserUseCaseProps } from "./GetUser";
-import {
-  UserService,
-  UUIDInMemoryGeneratorService,
-} from "../../infra/services";
+import { GetUserUseCase } from "./GetUser";
+import { UserRepositoryData } from "../../data/repositories";
+
+const userRepository = new UserInMemoryRepository();
+
+const getUserUseCase = new GetUserUseCase({
+  userRepository,
+});
 
 describe("Get User Use Case", () => {
   it("should find an user correctly when pass existing user id", async () => {
-    const dataValidator = new YupUserValidator();
-    const userMapper = new UserMapper();
-    const userRepository = new UserInMemoryRepository();
-    const userService = new UserService({ userRepository });
-    const uuidGenerator = new UUIDInMemoryGeneratorService();
-
-    const createUserUseCaseProps: CreateUserUseCaseProps = {
-      dataValidator,
-      userMapper,
-      userRepository,
-      uuidGenerator,
-      userService,
-    };
-    const createUserUseCase = new CreateUserUseCase(createUserUseCaseProps);
-    const getUserUseCase = new GetUserUseCase({
-      userRepository,
-    });
-
-    const inputData = {
+    const validUserInputData: UserRepositoryData = {
+      id: "valid_id",
       name: "valid_name",
       email: "email@email.com",
       password: "valid_password",
       avatar: "valid_avatar",
     };
-    const createdUserId = await createUserUseCase.exec(inputData);
-    const createdUser = await getUserUseCase.exec({ id: createdUserId });
+
+    userRepository.create(validUserInputData);
+
+    const createdUser = await getUserUseCase.exec({
+      id: validUserInputData.id,
+    });
+
+    userRepository.delete(validUserInputData.id);
 
     expect(createdUser).toBeTruthy();
-    expect(createdUserId).toEqual(createdUser.getId());
-    expect(inputData.name).toEqual(createdUser.getName());
-    expect(inputData.email).toEqual(createdUser.getEmail());
-    expect(inputData.password).toEqual(createdUser.getPassword());
-    expect(inputData.avatar).toEqual(createdUser.getAvatar());
+    expect(validUserInputData.id).toEqual(createdUser.getId());
+    expect(validUserInputData.name).toEqual(createdUser.getName());
+    expect(validUserInputData.email).toEqual(createdUser.getEmail());
+    expect(validUserInputData.password).toEqual(createdUser.getPassword());
+    expect(validUserInputData.avatar).toEqual(createdUser.getAvatar());
   });
 
   it("should throw not found exeception when try to find user with no existing ID", async () => {
-    const userRepository = new UserInMemoryRepository();
-
-    const getUserUseCaseProps: GetUserUseCaseProps = {
-      userRepository,
-    };
-
-    const getUserUseCase = new GetUserUseCase(getUserUseCaseProps);
     const ID = "no_existing_id";
 
     const error = await getThrowedErrorType(async () =>
