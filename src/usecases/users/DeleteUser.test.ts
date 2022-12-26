87af1ad06383c910112ecbcf4a935a193583f87d
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-12-26 20:06:06 -0300

    refactoring delete user use case unit test

--- src/usecases/users/DeleteUser.test.ts
+++ src/usecases/users/DeleteUser.test.ts
@@ -1,64 +1,39 @@
-import { UserMapper, YupUserValidator } from "../../infra/adapters";
 import { UserInMemoryRepository } from "../../infra/repositories";
-import { CreateUserUseCase, CreateUserUseCaseProps } from "./CreateUser";
 import { GetUserUseCase } from "./GetUser";
-import {
-  UserService,
-  UUIDInMemoryGeneratorService,
-} from "../../infra/services";
 import { DeleteUserUseCase } from "./DeleteUser";
 import { getThrowedErrorType, NoErrorThrownError } from "../../tests";
 import { NotFoundException } from "../../domain/exceptions";
+import { UserRepositoryData } from "../../data/repositories";
+
+const userRepository = new UserInMemoryRepository();
+
+const getUserUseCase = new GetUserUseCase({
+  userRepository,
+});
+
+const deleteUserUseCase = new DeleteUserUseCase({
+  getUserUseCase,
+  userRepository,
+});
+
+const userValidData: UserRepositoryData = {
+  id: "valid_id",
+  name: "valid_name",
+  email: "email@email.com",
+  password: "valid_password",
+  avatar: "valid_avatar",
+};
 
 describe("Delete User Use Case", () => {
   it("should delete an user correctly when pass existing user id", async () => {
-    const dataValidator = new YupUserValidator();
-    const userMapper = new UserMapper();
-    const userRepository = new UserInMemoryRepository();
-    const userService = new UserService({ userRepository });
-    const uuidGenerator = new UUIDInMemoryGeneratorService();
+    userRepository.create(userValidData);
+    await deleteUserUseCase.exec({ id: userValidData.id });
 
-    const createUserUseCaseProps: CreateUserUseCaseProps = {
-      dataValidator,
-      userMapper,
-      userRepository,
-      uuidGenerator,
-      userService,
-    };
-    const createUserUseCase = new CreateUserUseCase(createUserUseCaseProps);
-    const getUserUseCase = new GetUserUseCase({
-      userRepository,
-    });
-
-    const inputData = {
-      name: "valid_name",
-      email: "email@email.com",
-      password: "valid_password",
-      avatar: "valid_avatar",
-    };
-    const createdUserId = await createUserUseCase.exec(inputData);
-
-    const deleteUserUseCase = new DeleteUserUseCase({
-      getUserUseCase,
-      userRepository,
-    });
-
-    await deleteUserUseCase.exec({ id: createdUserId });
-
-    const error = await getThrowedErrorType(() =>
-      getUserUseCase.exec({ id: createdUserId })
-    );
-
-    expect(error).not.toBeInstanceOf(NoErrorThrownError);
-    expect(error).toBeInstanceOf(NotFoundException);
+    const removedUser = await userRepository.findById(userValidData.id);
+    expect(removedUser).toBeFalsy();
   });
 
   it("should throw not not found exception when pass a not existing user id", async () => {
-    const userRepository = new UserInMemoryRepository();
-    const getUserUseCase = new GetUserUseCase({
-      userRepository,
-    });
-
     const deleteUserUseCase = new DeleteUserUseCase({
       getUserUseCase,
       userRepository,
