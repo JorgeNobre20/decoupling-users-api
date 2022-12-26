7e58523d10ab0cd7a4bfa597f7d4cf8ade2df1f8
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-12-26 19:47:20 -0300

    refactoring create user use case unit test

--- src/usecases/users/CreateUser.test.ts
+++ src/usecases/users/CreateUser.test.ts
@@ -6,68 +6,52 @@ import {
   BusinessRuleException,
   InvalidDataException,
 } from "../../domain/exceptions";
-import { GetUserUseCase } from "./GetUser";
 import {
   UserService,
   UUIDInMemoryGeneratorService,
 } from "../../infra/services";
 
+const dataValidator = new YupUserValidator();
+const userMapper = new UserMapper();
+const userRepository = new UserInMemoryRepository();
+const userService = new UserService({
+  userRepository,
+});
+const uuidGenerator = new UUIDInMemoryGeneratorService();
+
+const createUserUseCaseProps: CreateUserUseCaseProps = {
+  dataValidator,
+  userMapper,
+  userRepository,
+  uuidGenerator,
+  userService,
+};
+const createUserUseCase = new CreateUserUseCase(createUserUseCaseProps);
+
+const firstUserCreationInputData = {
+  name: "valid_name",
+  email: "email@email.com",
+  password: "valid_password",
+  avatar: "valid_avatar",
+};
+
 describe("Create User Use Case", () => {
   it("should create user correctly when pass valid data", async () => {
-    const dataValidator = new YupUserValidator();
-    const userMapper = new UserMapper();
-    const userRepository = new UserInMemoryRepository();
-    const userService = new UserService({
-      userRepository,
-    });
-    const uuidGenerator = new UUIDInMemoryGeneratorService();
+    const createdUserId = await createUserUseCase.exec(
+      firstUserCreationInputData
+    );
+    const createdUser = await userRepository.findById(createdUserId);
+    await userRepository.delete(createdUserId);
 
-    const createUserUseCaseProps: CreateUserUseCaseProps = {
-      dataValidator,
-      userMapper,
-      userRepository,
-      uuidGenerator,
-      userService,
-    };
-    const createUserUseCase = new CreateUserUseCase(createUserUseCaseProps);
-    const getUserUseCase = new GetUserUseCase({
-      userRepository,
-    });
-
-    const inputData = {
-      name: "valid_name",
-      email: "email@email.com",
-      password: "valid_password",
-      avatar: "valid_avatar",
-    };
-    const createdUserId = await createUserUseCase.exec(inputData);
-    const createdUser = await getUserUseCase.exec({ id: createdUserId });
-
-    expect(createdUserId).toEqual(createdUser.getId());
-    expect(inputData.name).toEqual(createdUser.getName());
-    expect(inputData.email).toEqual(createdUser.getEmail());
-    expect(inputData.password).toEqual(createdUser.getPassword());
-    expect(inputData.avatar).toEqual(createdUser.getAvatar());
+    expect(createdUser).toBeTruthy();
+    expect(createdUserId).toEqual(createdUser?.id);
+    expect(firstUserCreationInputData.name).toEqual(createdUser?.name);
+    expect(firstUserCreationInputData.email).toEqual(createdUser?.email);
+    expect(firstUserCreationInputData.password).toEqual(createdUser?.password);
+    expect(firstUserCreationInputData.avatar).toEqual(createdUser?.avatar);
   });
 
   it("should throw error when pass invalid data", async () => {
-    const dataValidator = new YupUserValidator();
-    const userMapper = new UserMapper();
-    const userRepository = new UserInMemoryRepository();
-    const userService = new UserService({
-      userRepository,
-    });
-    const uuidGenerator = new UUIDInMemoryGeneratorService();
-
-    const createUserUseCaseProps: CreateUserUseCaseProps = {
-      dataValidator,
-      userMapper,
-      userRepository,
-      uuidGenerator,
-      userService,
-    };
-    const createUserUseCase = new CreateUserUseCase(createUserUseCaseProps);
-
     const input = {
       name: "",
       email: "invalid",
@@ -84,34 +68,14 @@ describe("Create User Use Case", () => {
   });
 
   it("should throw error when try create user with already existing email", async () => {
-    const dataValidator = new YupUserValidator();
-    const userMapper = new UserMapper();
-    const userRepository = new UserInMemoryRepository();
-    const userService = new UserService({
-      userRepository,
-    });
-    const uuidGenerator = new UUIDInMemoryGeneratorService();
-
-    const createUserUseCaseProps: CreateUserUseCaseProps = {
-      dataValidator,
-      userMapper,
-      userRepository,
-      uuidGenerator,
-      userService,
-    };
-    const createUserUseCase = new CreateUserUseCase(createUserUseCaseProps);
-
-    const firstInputData = {
-      name: "valid_name",
-      email: "email@email.com",
-      password: "valid_password",
-      avatar: "valid_avatar",
-    };
-
-    await createUserUseCase.exec(firstInputData);
-    const error = await getThrowedErrorType(async () =>
-      createUserUseCase.exec(firstInputData)
+    const createdUserId = await createUserUseCase.exec(
+      firstUserCreationInputData
     );
+    const error = await getThrowedErrorType(async () =>
+      createUserUseCase.exec(firstUserCreationInputData)
+    );
+
+    await userRepository.delete(createdUserId);
 
     expect(error).not.toBeInstanceOf(NoErrorThrownError);
     expect(error).toBeInstanceOf(BusinessRuleException);
