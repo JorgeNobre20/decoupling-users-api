d20be25d718f8ce30b37e7818cea5829d45c7e2e
JorgeNobre20 <jorgenobre1234@gmail.com>
2022-12-26 00:04:06 -0300

    add get throwed error type helper function test

---
+++ src/tests/getThrowedErrorType.test.ts
@@ -0,0 +1,15 @@
+import { getThrowedErrorType, NoErrorThrownError } from "./getThrowedErrorType";
+
+describe("Get Throwed Error Type Helper Function", () => {
+  it("should return NoErrorThrownError when everything runs correctly", async () => {
+    const error = await getThrowedErrorType(() => console.log("OK"));
+    expect(error).toBeInstanceOf(NoErrorThrownError);
+  });
+
+  it("should not return NoErrorThrownError when some error ocurred", async () => {
+    const error = await getThrowedErrorType(() => {
+      throw new Error("Error");
+    });
+    expect(error).not.toBeInstanceOf(NoErrorThrownError);
+  });
+});
